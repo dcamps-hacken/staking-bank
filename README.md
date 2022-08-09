ea09389aa2ffed1fa55eb9041cd90a5ad3e7e8de
fields93 <davidcamps67@gmail.com>
2022-08-09 11:37:38 +0200

    reviewed updateStatus(), added events and modifiers

--- README.md
+++ README.md
@@ -1,13 +1,35 @@
-# Sample Hardhat Project
+# Bank smart contract task
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+Your task is to create a bank smart contract which will enable anyone to deposit an amount X of XYZ
+ERC20 tokens to their savings (staking) account. The bank smart contract also contains an additional
+token reward pool of R XYZ tokens, deposited to the contract by the contract owner (bank owner) at
+contract deployment. At deployment the bank owner sets a time period constant T, to be used for reward
+calculation.
 
-Try running some of the following tasks:
+Contract dynamics (example illustrated below):
 
-```shell
-npx hardhat help
-npx hardhat test
-GAS_REPORT=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.js
-```
+✅ The smart contract is deployed at t0
+✅ The reward pool R is split into 3 subpools
+✅ R1 = 20% of R, available after 2T has passed since contract deployment
+✅ R2 = 30% of R, available after 3T has passed since contract deployment
+✅ R3 = 50% of R, available after 4T has passed since contract deployment
+✅ Deposit period: During the first period of T time the users can deposit tokens. After T has
+passed, no more deposits are allowed.
+✅ Lock period: From moment t0+T to t0+2T, users cannot withdraw their tokens (If the user tries to
+remove tokens before T time has elapsed since they have deposited, the transaction should
+fail).
+✅ Withdraw periods: After T2 has passed since contract deployment, the users can withdraw their
+tokens. However, the longer they wait, the bigger the reward they get
+✅ If a user withdraws tokens during the period t0+2T to t0+3T, they collect a proportional
+amount of the reward pool R1, according to the ratio of the number of tokens they have
+staked compared to the total number of tokens staked on the contract (by all users).
+✅ If a user withdraws tokens during the period t0+3T to t0+4T, they collect a proportional
+amount of the remaining reward pool R1 and R2, according to the proportion of the
+number of tokens they have staked compared to the total number of tokens staked on
+the contract (by all users)
+✅ If the user withdraws tokens after 4T has passed since contract deployment, they can
+receive the full reward of R (R1+R2+R3) proportionally to their ratio of tokens in the
+total pool
+✅ If no user waits for the last period (for 4T to pass), the remaining tokens on the
+contract can be withdrawn by the bank (contract owner). In no other situation can the
+bank owner remove tokens from the contract.
--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -25,6 +25,19 @@ contract Bank is Ownable, ReentrancyGuard {
     mapping(address => uint256) public balances;
     uint256 public stake;
 
+    event StatusUpdate(BankStatus newStatus);
+    event Deposit(address indexed user, uint256 indexed amount);
+    event Withdrawal(
+        address indexed user,
+        uint256 indexed amount,
+        uint256 rewardTokens
+    );
+
+    modifier checkStatus() {
+        _updateStatus();
+        _;
+    }
+
     constructor(
         uint256 _period,
         uint256 _reward,
@@ -42,44 +55,63 @@ contract Bank is Ownable, ReentrancyGuard {
 
     receive() external payable {}
 
-    function deposit(uint256 _amount) external nonReentrant {
+    function deposit(uint256 _amount) external nonReentrant checkStatus {
         require(status == BankStatus.DEPOSIT, "Deposit period has passed!");
         IERC20(token).transfer(address(this), _amount);
         balances[msg.sender] += _amount;
         stake += balances[msg.sender];
+        emit Deposit(msg.sender, _amount);
     }
 
-    function withdraw() external nonReentrant {
+    function withdraw() external nonReentrant checkStatus {
         require(
             status != BankStatus(0) && status != BankStatus(1),
             "Withdrawals not available yet!"
         );
         require(balances[msg.sender] > 0, "No tokens to withdraw!");
         uint256 yield = R * (balances[msg.sender] / stake);
+        uint256 balance = balances[msg.sender];
         stake -= balances[msg.sender];
         balances[msg.sender] = 0;
-        IERC20(token).transfer(msg.sender, balances[msg.sender] + yield);
+        IERC20(token).transfer(msg.sender, balance + yield);
+        emit Withdrawal(msg.sender, balance, yield);
     }
 
     function recall() external onlyOwner {
         require(status == BankStatus.THIRD_UNLOCK);
-        require(stake == 0);
+        require(stake == 0); // can it actually be 0?
         IERC20(token).transfer(msg.sender, R);
     }
 
-    function updateStatus() public onlyOwner {
+    function _updateStatus() private onlyOwner {
         // Make sure all are whole numbers!!
-        if (block.timestamp < (t0 + T)) {
-            status = BankStatus.LOCK;
-        } else if (block.timestamp > (t0 + 2 * T)) {
-            status = BankStatus.FIRST_UNLOCK;
-            R += (reward * 2) / 10;
-        } else if (block.timestamp > (t0 + 3 * T)) {
-            status = BankStatus.SECOND_UNLOCK;
-            R += (reward * 3) / 10;
-        } else if (block.timestamp > (t0 + 4 * T)) {
-            status = BankStatus.THIRD_UNLOCK;
-            R += (reward * 5) / 10;
+        if (block.timestamp <= (t0 + T)) {
+            if (status != BankStatus.LOCK) {
+                status = BankStatus.LOCK;
+                emit StatusUpdate(status);
+            }
+        } else if (
+            (t0 + 2 * T) <= block.timestamp && block.timestamp < (t0 + 3 * T)
+        ) {
+            if (status != BankStatus.FIRST_UNLOCK) {
+                status = BankStatus.FIRST_UNLOCK;
+                emit StatusUpdate(status);
+                R += (reward * 2) / 10;
+            }
+        } else if (
+            (t0 + 3 * T) <= block.timestamp && block.timestamp < (t0 + 4 * T)
+        ) {
+            if (status != BankStatus.SECOND_UNLOCK) {
+                status = BankStatus.SECOND_UNLOCK;
+                emit StatusUpdate(status);
+                R += (reward * 3) / 10;
+            }
+        } else {
+            if (status != BankStatus.THIRD_UNLOCK) {
+                status = BankStatus.THIRD_UNLOCK;
+                emit StatusUpdate(status);
+                R += (reward * 5) / 10;
+            }
         }
     }
 }
